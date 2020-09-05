039acf772f05af843a976acdb4c6b2c41f175342
Stevan <stevan_stetz@hotmail.com>
2020-09-05 13:30:06 -0300

    1.0

---
+++ bot.js
@@ -0,0 +1,64 @@
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: 'stevan_stetz',
+    password: 'w2disxaso56gin0qzkdpl6hrei3t8k'
+  },
+  channels: [
+    'freljhordan',
+    'missleblanc',
+    'kiwionline'
+  ]
+};
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === 'LUL') {
+    client.say(target, `LUL`);
+    console.log(`* Executed ${commandName} command`);
+  } 
+  else if (commandName === 'Kappa') {
+    client.say(target, `Kappa`);
+    console.log(`* Executed ${commandName} command`);
+  } 
+  else if (commandName === 'KEKW') {
+    client.say(target, `KEKW`);
+    console.log(`* Executed ${commandName} command`);
+  }
+  else if (commandName === 'OMEGALUL') {
+    client.say(target, `OMEGALUL`);
+    console.log(`* Executed ${commandName} command`);
+  }
+  else if (commandName === 'eu tu nos') {
+    client.say(target, `sub nelas`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
